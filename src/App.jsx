b612827ac909d2045e573f324bf31bbbbcb0fca3
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-09-27 17:16:41 +0530

    Registration Basic

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Register from './Pages/Register'
 import Login from './Pages/Login'
+import { b, div } from 'framer-motion/client'
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/register">Register</a></li>
-          <li><a href="/login">Login</a></li>
-        </ul>
-      </nav>
+    <div className='container mx-auto bg-gray-400'>
+      <Router>
+        <nav className="p-3 space-x-4">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded"><a href="/">Home</a></button>
+          <button className="px-4 py-2 bg-green-500 text-white rounded"><a href="/register">Register</a></button>
+          <button className="px-4 py-2 bg-red-500 text-white rounded"><a href="/login">Login</a></button>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
